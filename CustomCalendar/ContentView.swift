3b3519a2b1b64b3ea195e0836155b6c484df3829
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2024-04-30 14:32:33 -0300

    Initial Commit

---
+++ CustomCalendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomCalendar
+//
+//  Created by Laura C. Balbachan dos Santos on 30/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomCalendar/CustomCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalendarApp.swift
+//  CustomCalendar
+//
+//  Created by Laura C. Balbachan dos Santos on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
